a576a1d8fdb76fd2245dde64d5b840824c1e81c5
Michael Kim <mkim0407@gmail.com>
2023-02-10 14:32:20 -0800

    add feed list operations
    https://www.howtographql.com/graphql-js/8-filtering-pagination-and-sorting/

--- backend2/src/resolvers/Query.js
+++ backend2/src/resolvers/Query.js
@@ -2,8 +2,30 @@ function info() {
     return `This is the API of a Hackernews Clone`;
 }
 
-function feed(root, {}, {prisma}) {
-    return prisma.link.findMany();
+function feed(root, {filter, skip, take, orderBy}, {prisma}) {
+    const where = filter
+        ? {
+            OR: [
+                {description: {contains: filter}},
+                {url: {contains: filter}},
+            ],
+        }
+        : {};
+    const links = prisma.link
+        .findMany({
+            where,
+            skip,
+            take,
+            orderBy,
+        });
+    const count = prisma.link
+        .count({
+            where,
+        });
+    return {
+        links,
+        count,
+    };
 }
 
 function link(root, {id}, {prisma}) {
