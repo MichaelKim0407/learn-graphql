9cd6f0290b9ccb2889938d73a15bc3f065ac9247
Michael Kim <mkim0407@gmail.com>
2023-02-10 17:03:23 -0800

    add author and vote info
    https://www.howtographql.com/react-apollo/6-more-mutations-and-updating-the-store/

--- frontend/src/components/Link.js
+++ frontend/src/components/Link.js
@@ -1,11 +1,22 @@
 import React from 'react';
+import {timeDifferenceForDate} from "../utils/time";
 
 const Link = (props) => {
     const {link} = props;
+
     return (
-        <div>
-            <div>
-                {link.description} ({link.url})
+        <div className="flex mt2 items-start">
+            <div className="ml1">
+                <div>
+                    {link.description} ({link.url})
+                </div>
+                {(
+                    <div className="f6 lh-copy gray">
+                        {link.votes.length} votes | by{' '}
+                        {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+                        {timeDifferenceForDate(link.createdAt)}
+                    </div>
+                )}
             </div>
         </div>
     );
